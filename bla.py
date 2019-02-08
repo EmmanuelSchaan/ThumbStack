d3eccf5950e56ca61ed11d70e5106d074e790a1d
EmmanuelSchaan <emmanuel.schaan@gmail.com>
2019-02-07 18:58:44 -0800

    sky2map

--- bla.py
+++ bla.py
@@ -1,16 +1,15 @@
+# check the hit map as a function of dec, at fixed ra = 0
+def sky2map(dec, ra=45.):
+   yPix,xPix = enmap.sky2pix(bigmap.shape, bigmap.wcs, np.array([dec,ra])*utils.degree)
+   # round to closest pixel
+   yPix = np.int(np.round(yPix)) % bigmap.shape[0]
+   xPix = np.int(np.round(xPix)) % bigmap.shape[1]
+   # evaluate the map at the desired pixel
+   return bigmap[yPix, xPix]
 
 
-
-
-dec = 20 ; ra = 180
-coords = np.deg2rad(np.array((dec,ra)))
-ypix,xpix = enmap.sky2pix(shape,wcs,coords)
-
-bigmap[ypix, xpix]#???? Is this the right order?
-
-
-ra = 20. # deg
-dec = 3. # deg
-bigmap.at(np.array([ra, dec])*utils.degree)
-
-
+# check the hit map as a function of dec, at fixed ra = 0
+def sky2map(dec, ra=45.):
+   # interpolate the map to the given sky coordinates
+   sourcecoord = np.array([dec, ra])*utils.degree
+   return bigmap.at(sourcecoord, prefilter=False, mask_nan=False)
--- code_snippet_enlib.py
+++ code_snippet_enlib.py
@@ -22,18 +22,88 @@ pathMask = pathIn + "f150_mask_foot_planck_ps_car.fits"
 # load a healpy map
 bigmap = enmap.read_map(pathHit)
 
+
+#######################################################################
+#######################################################################
+# Get map value at sky location: interpolated
+
 # setup the interpolation algorithm,
 # done once for all, to speed up subsequent calls
 bigmap = utils.interpol_prefilter(bigmap, inplace=True)
 
-
-##########################################################################
-
-# Get map value at specific location on the sky
+# specify sky coordinates
 ra = 22. # deg
 dec = 0. # deg
+
+# interpolate the map to the given sky coordinates
 sourcecoord = np.array([dec, ra])*utils.degree
 bigmap.at(sourcecoord, prefilter=False, mask_nan=False)
+
+
+#######################################################################
+# Get map value at sky location: closest pixel
+
+# specify sky coordinates
+ra = 22. # deg
+dec = -90. # deg
+
+yPix,xPix = enmap.sky2pix(bigmap.shape, bigmap.wcs, np.array([dec,ra])*utils.degree)
+
+# round to closest pixel
+yPix = np.int(np.round(yPix))
+xPix = np.int(np.round(xPix))
+
+print "ra, dec =", ra, dec
+print "x, y =", xPix, yPix
+print "nX, nY =", bigmap.shape[1], bigmap.shape[0]
+
+# evaluate the map at the desired pixel
+bigmap[yPix, xPix]
+
+
+
+#######################################################################
+# Testing them on the hit count map, to see typical hit values
+
+
+# check the hit map as a function of dec, at fixed ra = 0
+# this will return an error if the ra,dec value is outside the map
+def sky2map(dec, ra=45.):
+   yPix,xPix = enmap.sky2pix(bigmap.shape, bigmap.wcs, np.array([dec,ra])*utils.degree)
+   # round to closest pixel
+   yPix = np.int(np.round(yPix))
+   xPix = np.int(np.round(xPix))
+   # evaluate the map at the desired pixel
+   return bigmap[yPix, xPix]
+
+
+# check the hit map as a function of dec, at fixed ra = 0
+def sky2map(dec, ra=45.):
+   # interpolate the map to the given sky coordinates
+   sourcecoord = np.array([dec, ra])*utils.degree
+   return bigmap.at(sourcecoord, prefilter=False, mask_nan=False)
+
+
+
+Dec = np.linspace(-90., 90., 201)
+
+Hit = np.array(map(sky2map, Dec))
+
+fig=plt.figure(0)
+ax=fig.add_subplot(111)
+#
+ax.plot(Dec, Hit, ls='-')
+ax.plot(Dec, -Hit, ls='--')
+#
+ax.set_yscale('log', nonposx='clip')
+ax.set_xlabel(r'Dec')
+ax.set_ylabel(r'Hit')
+ax.set_xlim((-90., 90.))
+ax.set_ylim((1.e-5, 1.e-2))
+#
+fig.savefig("./test.pdf", bbox_inches='tight')
+fig.clf()
+
 
 
 
@@ -45,11 +115,13 @@ bigmap.at(sourcecoord, prefilter=False, mask_nan=False)
 
 
 
-
-
-
-
+#######################################################################
 ##########################################################################
+# Extract a square cutout of the map
+
+# setup the interpolation algorithm,
+# done once for all, to speed up subsequent calls
+bigmap = utils.interpol_prefilter(bigmap, inplace=True)
 
 # define geometry of small square maps to be extracted
 # here 1deg * 1deg, with 0.25arcmin pixel
